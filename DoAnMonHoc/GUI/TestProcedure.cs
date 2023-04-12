9b33ff13cd6687c2fefeebf27d5ffe96cb5604d8
Minh Sang <ngos810@gmail.com>
2023-04-12 19:00:35 +0700

    Sang update

--- DoAnMonHoc/GUI/TestProcedure.cs
+++ DoAnMonHoc/GUI/TestProcedure.cs
@@ -38,20 +38,20 @@ namespace DoAnMonHoc.GUI
         private void button2_Click(object sender, EventArgs e)
         {
             // load dữ liệu procedure select
-            //string strCon = @"DATA SOURCE = DELL:1521/ORCL.LOCALDOMAIN; USER ID=MINHSANG;PASSWORD=21062002";
-            //OracleConnection con = new OracleConnection(strCon);
-            //string queryString = "get_employees";
-            //DataTable dataTable = new DataTable();
-            //OracleCommand command = new OracleCommand(queryString, con);
-            //command.CommandType = CommandType.StoredProcedure;
-            //OracleParameter outCursorParam = new OracleParameter("out_cursor", OracleDbType.RefCursor);
-            //outCursorParam.Direction = ParameterDirection.Output;
-            //command.Parameters.Add(outCursorParam);
-            //OracleDataAdapter adapter = new OracleDataAdapter(command);
-            //con.Open();
-            //adapter.Fill(dataTable);
-            //con.Close();
-            //dataGridView1.DataSource = dataTable;
+            string strCon = @"DATA SOURCE = DELL:1521/ORCL.LOCALDOMAIN; USER ID=MINHSANG;PASSWORD=21062002";
+            OracleConnection con = new OracleConnection(strCon);
+            string queryString = "proc_xuat_thongtin_nhanvien";
+            DataTable dataTable = new DataTable();
+            OracleCommand command = new OracleCommand(queryString, con);
+            command.CommandType = CommandType.StoredProcedure;
+            OracleParameter outCursorParam = new OracleParameter("out_cursor", OracleDbType.RefCursor);
+            outCursorParam.Direction = ParameterDirection.Output;
+            command.Parameters.Add(outCursorParam);
+            OracleDataAdapter adapter = new OracleDataAdapter(command);
+            con.Open();
+            adapter.Fill(dataTable);
+            con.Close();
+            dataGridView1.DataSource = dataTable;
 
             // thực hiện procedure insert
             //string strCon = @"DATA SOURCE = DELL:1521/ORCL.LOCALDOMAIN; USER ID=MINHSANG;PASSWORD=21062002";
@@ -73,20 +73,20 @@ namespace DoAnMonHoc.GUI
             //cmd.CommandType = CommandType.StoredProcedure;
             //cmd.CommandText = "proc_update_chucvu";
             //cmd.Parameters.Add("p_employee_id", OracleDbType.Char).Value = "CV005";
-            //cmd.Parameters.Add("p_first_name", OracleDbType.Varchar2).Value = "Thu Phí";
+            //cmd.Parametet_name", OracleDbType.Varchar2).Value = "Thu Phí";
             //cmd.ExecuteNonQuery();
             //con.Close();
 
             // thực hiện procedure delete
-            string strCon = @"DATA SOURCE = DELL:1521/ORCL.LOCALDOMAIN; USER ID=MINHSANG;PASSWORD=21062002";
-            OracleConnection con = new OracleConnection(strCon);
-            OracleCommand cmd = con.CreateCommand();
-            con.Open();
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.CommandText = "proc_delete_chucvu";
-            cmd.Parameters.Add("p_employee_id", OracleDbType.Char).Value = "CV005";
-            cmd.ExecuteNonQuery();
-            con.Close();
+            //string strCon = @"DATA SOURCE = DELL:1521/ORCL.LOCALDOMAIN; USER ID=MINHSANG;PASSWORD=21062002";
+            //OracleConnection con = new OracleConnection(strCon);
+            //OracleCommand cmd = con.CreateCommand();
+            //con.Open();
+            //cmd.CommandType = CommandType.StoredProcedure;
+            //cmd.CommandText = "proc_delete_chucvu";
+            //cmd.Parameters.Add("p_employee_id", OracleDbType.Char).Value = "CV005";
+            //cmd.ExecuteNonQuery();
+            //con.Close();
         }
     }
 }
