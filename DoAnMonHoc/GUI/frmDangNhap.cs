93d51c557640187ca170842700bee41a2cf67a8c
Minh Sang <ngos810@gmail.com>
2023-04-13 11:15:13 +0700

    Sang commit

--- DoAnMonHoc/GUI/frmDangNhap.cs
+++ DoAnMonHoc/GUI/frmDangNhap.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
 
 namespace DoAnMonHoc.GUI
 {
@@ -22,10 +23,31 @@ namespace DoAnMonHoc.GUI
         {
             string userName = txtUserName.Text;
             string passWord = txtPassword.Text;
-            DataTable table = helper.ExecuteQuery("select * from NHANVIEN where TENDANGNHAP='" + userName + "' and MATKHAU='" + passWord + "'");
-            if (table.Rows.Count <= 0)
-                MessageBox.Show("Sai mật khẩu hoặc tài khoản. Vui lòng đăng nhập lại!", "Thông báo", MessageBoxButtons.OK);
+            //DataTable table = helper.ExecuteQuery("select * from NHANVIEN where TENDANGNHAP='" + userName + "' and MATKHAU='" + passWord + "'");
+            //if (table.Rows.Count <= 0)
+            //    MessageBox.Show("Sai mật khẩu hoặc tài khoản. Vui lòng đăng nhập lại!", "Thông báo", MessageBoxButtons.OK);
+            //else
+            //{
+            //    frmMenu menu = new frmMenu();
+            //    MessageBox.Show("Đăng nhập thành công", "Thông báo", MessageBoxButtons.OK);
+            //    this.Visible = false;
+            //    menu.ShowDialog();
+            //}
 
+            string strCon = @"DATA SOURCE = DELL:1521/ORCL.LOCALDOMAIN; USER ID=MINHSANG;PASSWORD=21062002";
+            OracleConnection con = new OracleConnection(strCon);
+            con.Open();
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "func_kiemtradangnhap";
+            cmd.Parameters.Add("tendangnhap", OracleDbType.Char).Value = userName;
+            cmd.Parameters.Add("matkhau", OracleDbType.Char).Value = passWord;
+            cmd.Parameters.Add("check", OracleDbType.Boolean).Direction = ParameterDirection.ReturnValue;
+            cmd.ExecuteNonQuery();
+            Boolean check = Convert.ToBoolean(cmd.Parameters["check"].Value);
+            if (check == false)
+                MessageBox.Show("Sai mật khẩu hoặc tài khoản. Vui lòng đăng nhập lại!", "Thông báo", MessageBoxButtons.OK);
             else
             {
                 frmMenu menu = new frmMenu();
@@ -33,7 +55,6 @@ namespace DoAnMonHoc.GUI
                 this.Visible = false;
                 menu.ShowDialog();
             }
-                
         }
     }
 }
--- DoAnMonHoc/Program.cs
+++ DoAnMonHoc/Program.cs
@@ -19,7 +19,7 @@ namespace DoAnMonHoc
             //Application.Run(new GUI.frmHuongDanKetNoiDB());
 
             //Application.Run(new GUI.FormDonHang());
-            Application.Run(new GUI.TestProcedure());
+            Application.Run(new GUI.frmDangNhap());
         }
     }
 }
